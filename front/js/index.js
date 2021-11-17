14b0fa269b2a6ecf4e73fc0eea62cea63b320ae1
Julien Lec <julienlecornu@gmail.com>
2021-11-17 14:46:35 +0000

    Affichage des produits sur la home

---
+++ front/js/index.js
@@ -0,0 +1,20 @@
+fetch('http://localhost:3000/api/products/') // promise: wait until resolve/reject 
+    .then(response => response.json())
+    .then(json => afficheCanape(json)
+);
+
+// AFFICHE LES CARDS DES CANAPES : LIEN + IMG + NOM + DESCRIPTION
+function afficheCanape(data) {
+    for (let i = 0; i < data.length; i++) {
+
+        document.querySelector('#items').innerHTML += `
+            <a href="./product.html?id=${data[i]['_id']}">
+                <article>
+                    <img src="${data[i]['imageUrl']}" alt="${data[i]['altTxt']}">
+                    <h3 class="productName">${data[i]['name']}</h3>
+                    <p class="productDescription">${data[i]['description']}</p>
+                </article>
+            </a>
+        `
+    }
+}
